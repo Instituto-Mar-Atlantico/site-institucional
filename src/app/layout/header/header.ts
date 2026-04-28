bca9205003c1bd47b76a809266854618f7680316
Matheus Barraboni Cabrini <matheusbarraboni@gmail.com>
2026-04-28 13:42:09 -0300

    fix: toogle theme

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { MatButton, MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatToolbar } from '@angular/material/toolbar';
@@ -10,15 +10,20 @@ import { MatToolbar } from '@angular/material/toolbar';
   styleUrl: './header.scss',
 })
 export class Header {
+  readonly isDarkTheme = signal(document.documentElement.getAttribute('data-theme') === 'dark');
+  readonly themeIcon = computed(() => this.isDarkTheme() ? 'light_mode' : 'dark_mode');
+  readonly themeAriaLabel = computed(() => this.isDarkTheme() ? 'Ativar tema claro' : 'Ativar tema escuro');
+
   toggleTheme() {
     const root = document.documentElement;
+    const nextIsDark = !this.isDarkTheme();
 
-    const isDark = root.getAttribute('data-theme') === 'dark';
-  
-    if (isDark) {
-      root.removeAttribute('data-theme');
-    } else {
+    if (nextIsDark) {
       root.setAttribute('data-theme', 'dark');
+    } else {
+      root.removeAttribute('data-theme');
     }
+
+    this.isDarkTheme.set(nextIsDark);
   }
 }
